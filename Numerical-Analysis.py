20032d0b5c72510f5bfb3253c51e4a36a5bda23e
Hen Simkin <hensi1@ac.sce.ac.il>
2022-03-10 20:14:03 +0200

    The file contains question num 2,3,4

--- Numerical-Analysis.py
+++ Numerical-Analysis.py
@@ -1,2 +1,21 @@
+
+import numpy as np
+print(np.finfo(float).eps)
+print(np.finfo(np.float32).eps)
+"""""
+The first functions represents the differnce between floating point as machine prescion
+"""""
+
+def maachineEpsilon(eps):
+    while(1+eps)!=1:
+        prev_eps=eps
+        eps = eps / 2
+    print("machine epsilon is: ",prev_eps)
+
+
+maachineEpsilon(0.5)
+
+
 print(abs(3.0*(4.0/3.0-1)-1))
-print("check")
+
+print(round(abs(3.0*(4.0/3.0-1)-1)))
