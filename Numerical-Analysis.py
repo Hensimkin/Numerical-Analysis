c358758a5a89f2b5e9ddda74a05ebb16d5987f98
barmor12 <77288623+barmor12@users.noreply.github.com>
2022-03-12 18:26:30 +0200

    Update Numerical-Analysis.py

--- Numerical-Analysis.py
+++ Numerical-Analysis.py
@@ -10,11 +10,9 @@ def maachineEpsilon(eps):
     while(1+eps)!=1:
         prev_eps=eps
         eps = eps / 2
-    print("machine epsilon is: ",prev_eps)
-
+    print("machine epsilon is:",prev_eps)
 
 maachineEpsilon(0.5)
-
 
 print(abs(3.0*(4.0/3.0-1)-1))
 
